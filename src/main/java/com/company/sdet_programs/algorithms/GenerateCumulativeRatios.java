b59fd9124b1e1a0971719dc32adbdf50cf8c3ca0
mraphael2101 <mark.raphael@ymail.com>
2023-08-17 22:15:53 +0100

    WIP

--- src/main/java/com/company/sdet_programs/algorithms/GenerateCumulativeRatios.java
+++ src/main/java/com/company/sdet_programs/algorithms/GenerateCumulativeRatios.java
@@ -15,7 +15,7 @@ public class GenerateCumulativeRatios {
     | 150-199 | 0.25  | 1                |
     */
     public static void main(String[] args) {
-        ArrayList<Integer> boundaries = createArray(300);
+        ArrayList<Integer> boundaries = createBoundaryValues(200);
         System.out.println(boundaries);
 
         double[] cumulativeRatios = generateCumulativeRatios(boundaries);
@@ -23,14 +23,14 @@ public class GenerateCumulativeRatios {
     }
 
     // A method that takes a max value parameter and returns an array list of values
-    public static ArrayList<Integer> createArray(int max) {
+    public static ArrayList<Integer> createBoundaryValues(int max) {
         ArrayList<Integer> list = new ArrayList<Integer>();
         list.add(10);
-        list.add(20);
-        list.add(50);
-        list.add(100);
-        list.add(150);
-        list.add(200);
+//        list.add(20);
+//        list.add(50);
+//        list.add(100);
+//        list.add(150);
+//        list.add(200);
         // Add increments of 50 until the max value is reached or exceeded
         int value = 250;
         while (value <= max) {
@@ -41,41 +41,45 @@ public class GenerateCumulativeRatios {
     }
 
     public static double[] generateCumulativeRatios(ArrayList<Integer> boundaries) {
-        // Initialize an array to store the cumulative ratios
         double[] cumulativeRatios = new double[boundaries.size() + 1];
         // Set the first element to zero
         cumulativeRatios[0] = 0.0;
-        // Initialize a variable to store the sum of the divisors
-        double divisorSum = 0.0;
-        // Loop through the boundaries array list and calculate the divisors
+        // Initialize a variable to store the sum of the boundary differences
+        double differenceSum = getDifferenceSum(boundaries);
+        // Loop through the boundaries array list and calculate the cumulative ratios
         for (int i = 0; i < boundaries.size(); i++) {
-            int boundary = boundaries.get(i);
-            double divisor;
-            // Assign the divisor based on the boundary value
-            if (boundary == 10) {
-                divisor = 0.05;
-            } else if (boundary >= 10 && boundary <= 20) {
-                divisor = 0.05;
-            } else if (boundary > 20 && boundary <= 50) {
-                divisor = 0.15;
-            } else if (boundary > 50 && boundary <= 100) {
-                divisor = 0.25;
-            } else if (boundary > 100 && boundary <= 150) {
-                divisor = 0.25;
-            } else if (boundary > 150 && boundary <= 200) {
-                divisor = 0.25;
-            } else {
-                // For any other boundary value, use increments of 0.25
-                divisor = 0.25;
-            }
-            // Add the divisor to the sum
-            divisorSum += divisor;
+            double difference = getDifference(boundaries, i);
             // Calculate the cumulative ratio for the current level and store it in the array
-            cumulativeRatios[i + 1] = divisorSum;
+            cumulativeRatios[i + 1] = cumulativeRatios[i] + (difference / differenceSum);
         }
-        // Set the last element to one
-        cumulativeRatios[cumulativeRatios.length - 1] = 1.0;
+
         return cumulativeRatios;
+    }
+
+    private static double getDifference(ArrayList<Integer> boundaries, int i) {
+        int boundary = boundaries.get(i);
+        double difference;
+        // Assign the difference based on the boundary value and the previous boundary value
+        if (i == 0) {
+            // For the first boundary, use the boundary value itself
+            difference = boundary;
+        } else {
+            // For any other boundary, use the difference between the current and the previous boundary values
+            difference = boundary - boundaries.get(i - 1);
+        }
+        return difference;
+    }
+
+    private static double getDifferenceSum(ArrayList<Integer> boundaries) {
+        double differenceSum = 0.0;
+        // Loop through the boundaries array list and calculate the difference sum
+        for (int i = 0; i < boundaries.size(); i++) {
+            // Get the current boundary value
+            double difference = getDifference(boundaries, i);
+            // Add the difference to the sum
+            differenceSum += difference;
+        }
+        return differenceSum;
     }
 
 }
