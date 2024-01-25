49909021557a0807730f1f981519f3597575d294
mraphael2101ba <mark.raphael@ba.com>
2024-01-25 09:06:04 +0000

    WIP

--- src/main/java/com/company/sdet_programs/countPojos/CountCustomPojo.java
+++ src/main/java/com/company/sdet_programs/countPojos/CountCustomPojo.java
@@ -4,16 +4,16 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-record StepPathNameAndRowIndex(String path, String stepName, int rowIndex) {}
+record StepKeyConstituents(String path, String stepName, int rowIndex) {}
 
 public class CountCustomPojo {
-    static ArrayList<StepPathNameAndRowIndex> data = new ArrayList<>();
+    static ArrayList<StepKeyConstituents> data = new ArrayList<>();
 
     static {
-        data.add(new StepPathNameAndRowIndex("dir/myfile1", "mark", 23));
-        data.add(new StepPathNameAndRowIndex("dir/myfile2", "mark", 23));
-        data.add(new StepPathNameAndRowIndex("dir/myfile3", "john", 27));
-        data.add(new StepPathNameAndRowIndex("dir/myfile3", "alex", 29));
+        data.add(new StepKeyConstituents("dir/myfile1", "mark", 23));
+        data.add(new StepKeyConstituents("dir/myfile2", "mark", 23));
+        data.add(new StepKeyConstituents("dir/myfile3", "john", 27));
+        data.add(new StepKeyConstituents("dir/myfile3", "alex", 29));
 
     }
     public static void main(String[] args) {
@@ -25,9 +25,9 @@ public class CountCustomPojo {
         }
     }
 
-    public static Map<String, Integer> countNames(ArrayList<StepPathNameAndRowIndex> data) {
+    public static Map<String, Integer> countNames(ArrayList<StepKeyConstituents> data) {
         Map<String, Integer> nameCounts = new HashMap<>();
-        for (StepPathNameAndRowIndex item : data) {
+        for (StepKeyConstituents item : data) {
             String name = item.stepName();
             nameCounts.put(name, nameCounts.getOrDefault(name, 0) + 1);
         }
